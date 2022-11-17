0118fbf8383f3348952b4ae3221b88a9fa309811
VemulaPaavanaSaiKumar18 <vpskmuni18@gmail.com>
2022-11-17 19:13:04 +0530

    changes done

--- Assignment-07/domTable.js
+++ Assignment-07/domTable.js
@@ -6,12 +6,11 @@ const data = [
   { firstName: "Rama", lastName: "bai", age: 18, button: "Alert" },
   { firstName: "Raj", lastName: "v", age: 27, button: "Alert" },
 ];
-let table = document.createElement("table");
-let thead = document.createElement("thead");
-let tbody = document.createElement("tbody");
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
 
-table.appendChild(thead);
-table.appendChild(tbody);
+table.append(thead, tbody);
 
 // Adding the entire table to the body tag
 document.getElementById("body").appendChild(table);
@@ -27,13 +26,10 @@ heading_3.innerHTML = "Age";
 let heading_4 = document.createElement("th");
 heading_4.innerHTML = "Button";
 
-row_1.appendChild(heading_1);
-row_1.appendChild(heading_2);
-row_1.appendChild(heading_3);
-row_1.appendChild(heading_4);
+row_1.append(heading_1, heading_2, heading_3, heading_4);
 thead.appendChild(row_1);
 
-data.map((item) => {
+data.forEach((item) => {
   let row = document.createElement("tr");
   let rowData_1 = document.createElement("td");
   rowData_1.innerHTML = item.firstName;
@@ -45,10 +41,7 @@ data.map((item) => {
   rowButton.setAttribute("class", "button");
   rowButton.innerText = item.button;
 
-  row.appendChild(rowData_1);
-  row.appendChild(rowData_2);
-  row.appendChild(rowData_3);
-  row.appendChild(rowButton);
+  row.append(rowData_1, rowData_2, rowData_3, rowButton);
   tbody.appendChild(row);
 
   const alertBox = () => {
